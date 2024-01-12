bb0d819841fca818e845d74fd6f1679140980b8d
fuunshi <pranilshrestha2016@gmail.com>
2024-01-12 21:12:53 +0545

    Completed Admin and Managers

--- ArtOurs/ArtOurs/settings.py
+++ ArtOurs/ArtOurs/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
-    'utilities',
+    'custom_auth',
+    'core',
 ]
 
 MIDDLEWARE = [
--- ArtOurs/core/apps.py
+++ ArtOurs/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationConfig(AppConfig):
+class UtilitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'core'
