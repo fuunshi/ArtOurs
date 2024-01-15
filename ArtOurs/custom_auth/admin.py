e65f7b9a7a240de4863597657377782d9162feff
fuunshi <pranilshrestha2016@gmail.com>
2024-01-15 20:56:27 +0545

    Seperated Managers into different files

--- ArtOurs/custom_auth/admin.py
+++ ArtOurs/custom_auth/admin.py
@@ -1,40 +1,5 @@
 from django.contrib import admin
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .managers import CustomUser, CustomUserAdmin
 
-# class CustomUserCreationForm(UserCreationForm):
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email', 'username', 'is_artist')
-
-# class CustomUserChangeForm(UserChangeForm):
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email', 'username', 'is_artist', 'avatar', 'last_login')
-
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    # add_form = CustomUserCreationForm
-    # form = CustomUserChangeForm
-    list_display = ('email', 'username', 'is_artist', 'last_login')
-    list_filter = ('is_artist', 'groups', 'user_permissions')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
-
-    fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'is_artist', 'avatar')}),
-        ('Permissions', {'fields': ('groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2'),
-        }),
-    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- ArtOurs/custom_auth/views.py
+++ ArtOurs/custom_auth/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .managers import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after signup
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'art/signup.html', {'form': form})
+
+def login(request):
+    return render(request, 'custom_auth/login.html', {})
+
+def logout(request):
+    pass
