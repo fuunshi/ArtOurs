deef2d13b2d9a4a0c1e4b5c0a4451511bd98d925
fuunshi <pranilshrestha2016@gmail.com>
2024-01-25 20:16:34 +0545

    Artists App

--- ArtOurs/ArtOurs/urls.py
+++ ArtOurs/ArtOurs/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('custom_auth.urls')),
+    path('marketplace/', include('marketplace.urls')),
 ]
