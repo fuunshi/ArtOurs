86ef1a235cac7356bc6133ba64ac0abc71e1b250
fuunshi <pranilshrestha2016@gmail.com>
2024-02-06 20:54:54 +0545

    Profile

--- ArtOurs/artists/views.py
+++ ArtOurs/artists/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import DetailView, UpdateView
+from .models import ArtistProfile, Artwork
+from .forms import ArtistProfileForm
 
-# Create your views here.
+class ArtistProfileDetailView(DetailView):
+    model = ArtistProfile
+    template_name = 'artists/artist_profile_detail.html'
+    context_object_name = 'artist'
+
+class ArtistProfileUpdateView(UpdateView):
+    model = ArtistProfile
+    form_class = ArtistProfileForm
+    template_name = 'artists/artist_profile_edit.html'
+
+    def get_success_url(self):
+        return reverse('artists/artist-profile-detail', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Additional processing if needed
+        return response
--- ArtOurs/marketplace/urls.py
+++ ArtOurs/marketplace/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('products/<int:pk>/order/', OrderProductView.as_view(), name='order-product'),
-    path('artists/<int:artist_id>/review/', ReviewCreateView.as_view(), name='create-review'),
+    # path('artists/<int:artist_id>/review/', ReviewCreateView.as_view(), name='create-review'),
 
 ]
