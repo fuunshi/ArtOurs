4a68ab04b04b71b5836c3eb5f08ee654fe243fe3
fuunshi <pranilshrestha2016@gmail.com>
2024-02-28 21:02:25 +0545

    Relocation

--- ArtOurs/artists/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ArtistProfile, Artwork
-
-admin.site.register(ArtistProfile)
-admin.site.register(Artwork)
--- ArtOurs/artists/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ArtistProfile
-
-class ArtistProfileForm(forms.ModelForm):
-    class Meta:
-        model = ArtistProfile
-        fields = ['bio', 'portfolio_link', 'is_available_for_commission']
